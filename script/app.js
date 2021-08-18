86d6cc4a083b19e32626ea763626509eb03c9b46
Thibault D'Haese <thibault.dhaese@student.howest.be>
2021-08-18 13:29:12 +0200

    variable for icon false or check

--- script/app.js
+++ script/app.js
@@ -2,8 +2,6 @@ const apiKey = "cd4c67af652c44a78cc397d1a044320c";
 let nextBtn, backBtn;
 
 let showResult = queryResponse => {
-    // console.log(queryResponse);
-
     let title = queryResponse.recipes[0].title;
     let dishTypes = queryResponse.recipes[0].dishTypes;
     let glutenFree = String(queryResponse.recipes[0].glutenFree);
@@ -14,11 +12,6 @@ let showResult = queryResponse => {
     let servings = queryResponse.recipes[0].servings;
     let extendedIngredients = queryResponse.recipes[0].extendedIngredients;
 
-    // console.log(queryResponse);
-    // console.log(`healthScore: ${healthScore}`);
-    // console.log(extendedIngredients)
-
-    
 
     document.querySelector(".js-Title").innerHTML = `${title}`;
     document.querySelector(".js-Image").innerHTML = `<img src="${image}" alt="Here comes the image of the recipe" class="c-imagedetail js-Image ">`;
@@ -39,144 +32,48 @@ let showResult = queryResponse => {
 
 
 
+    const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#078215">
+                <path d="M0 0h24v24H0V0z" fill="none"/>
+                <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM17.99 9l-1.41-1.42-6.59 6.59-2.58-2.57-1.42 1.41 4 3.99z"/>
+            </svg>`;
+
+    const falseIcon = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#b31200">
+                <rect fill="none" height="24" width="24"/>
+                <path d="M19,19H5V5h14V19z M3,3v18h18V3H3z M17,15.59L15.59,17L12,13.41L8.41,17L7,15.59L10.59,12L7,8.41L8.41,7L12,10.59L15.59,7 L17,8.41L13.41,12L17,15.59z"/>
+            </svg>`
 
 
     if (glutenFree == "false")
     {
         document.querySelector(".js-GlutenFree").innerHTML = 
-            `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-                <rect fill="none" height="24" width="24"/>
-                <path d="M19,19H5V5h14V19z M3,3v18h18V3H3z M17,15.59L15.59,17L12,13.41L8.41,17L7,15.59L10.59,12L7,8.41L8.41,7L12,10.59L15.59,7 L17,8.41L13.41,12L17,15.59z"/>
-            </svg>
+            `${falseIcon}
             <p class="c-card-info__icontext">Gluten Free</p>`;
     }
     else
     {
         document.querySelector(".js-GlutenFree").innerHTML =
-            `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-                <path d="M0 0h24v24H0V0z" fill="none"/>
-                <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM17.99 9l-1.41-1.42-6.59 6.59-2.58-2.57-1.42 1.41 4 3.99z"/>
-            </svg>
+            `${checkIcon}
             <p class="c-card-info__icontext">Gluten Free</p>`; 
     }
     if (vegan == "false")
     {
         document.querySelector(".js-Vegan").innerHTML = 
-            `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-                <rect fill="none" height="24" width="24"/>
-                <path d="M19,19H5V5h14V19z M3,3v18h18V3H3z M17,15.59L15.59,17L12,13.41L8.41,17L7,15.59L10.59,12L7,8.41L8.41,7L12,10.59L15.59,7 L17,8.41L13.41,12L17,15.59z"/>
-            </svg>
+            `${falseIcon}
             <p class="c-card-info__icontext">Vegan</p>`
     }
     else
     {
         document.querySelector(".js-Vegan").innerHTML =
-            `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-                <path d="M0 0h24v24H0V0z" fill="none"/>
-                <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM17.99 9l-1.41-1.42-6.59 6.59-2.58-2.57-1.42 1.41 4 3.99z"/>
-            </svg>
+            `${checkIcon}
             <p class="c-card-info__icontext">Vegan</p>`; 
     }
     
     
 
-    if (healthScore == 0)
-    {
-
-    }
-    else if (healthScore >= 1 && healthScore < 5)
-    {
-        console.log("1 Ster");
-        document.querySelector(".js-Star").innerHTML += 
-            `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-                <g>
-                    <path d="M0,0h24v24H0V0z" fill="none"/>
-                    <path d="M0,0h24v24H0V0z" fill="none"/>
-                </g>
-                <g>
-                    <path d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z"/>
-                </g>
-            </svg>`
-        
-    }
-    else if (healthScore >= 5 && healthScore < 10)
-    {
-        console.log("2 Sterren");
-     
-        let star = 2
-        for (let i = 0; i < star; i++) 
-        {
-            document.querySelector(".js-Star").innerHTML += 
-                `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-                    <g>
-                        <path d="M0,0h24v24H0V0z" fill="none"/>
-                        <path d="M0,0h24v24H0V0z" fill="none"/>
-                    </g>
-                    <g>
-                        <path d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z"/>
-                    </g>
-                </svg>`
-        };
     
-    }
-    else if (healthScore >= 10 && healthScore < 15)
-    {
-        console.log("3 Sterren");
-     
-        let star = 3;
-        for (let i = 0; i < star; i++) 
-        {
-            document.querySelector(".js-Star").innerHTML += 
-                `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-                    <g>
-                        <path d="M0,0h24v24H0V0z" fill="none"/>
-                        <path d="M0,0h24v24H0V0z" fill="none"/>
-                    </g>
-                    <g>
-                        <path d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z"/>
-                    </g>
-                </svg>`
-        };
-    }
-    else if (healthScore >= 15 && healthScore < 20)
-    {
-        console.log("4 Sterren");
-     
-        let star = 4;
-        for (let i = 0; i < star; i++) 
-        {
-            document.querySelector(".js-Star").innerHTML += 
-                `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-                    <g>
-                        <path d="M0,0h24v24H0V0z" fill="none"/>
-                        <path d="M0,0h24v24H0V0z" fill="none"/>
-                    </g>
-                    <g>
-                        <path d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z"/>
-                    </g>
-                </svg>`
-        };
-    }
-    else if (healthScore == 20)
-    {
-        console.log("5 Sterren");
-     
-        let star = 5;
-        for (let i = 0; i < star; i++) 
-        {
-            document.querySelector(".js-Star").innerHTML += 
-                `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-                    <g>
-                        <path d="M0,0h24v24H0V0z" fill="none"/>
-                        <path d="M0,0h24v24H0V0z" fill="none"/>
-                    </g>
-                    <g>
-                        <path d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z"/>
-                    </g>
-                </svg>`
-        };
-    }
 
+
+    document.querySelector(".js-Ingredients").innerHTML = "";
     for (let i = 0; i < extendedIngredients.length; i++)
     {
         let amountIngredient = extendedIngredients[i].amount
@@ -202,6 +99,34 @@ function addEventListenerPage() {
 
     console.log(backBtn, nextBtn);
 }
+
+async function handleBack() {
+    const recipeHistory = localStorage.getItem("@recipeHistory");
+
+    let historyArr = [];
+    let historyPoint = parseInt(localStorage.getItem("@recipeHistoryPoint"));
+
+    if (recipeHistory !== null && historyPoint !== null) {
+        historyArr = JSON.parse(recipeHistory);
+        newHistoryPoint = historyPoint - 1;
+
+        if (newHistoryPoint === 0) {
+            localStorage.setItem("@recipeHistoryPoint", 0);
+            document.querySelector('.js-back').style.display = "none";
+        }
+        else {
+            localStorage.setItem("@recipeHistoryPoint", newHistoryPoint);
+            document.querySelector('.js-back').style.display = "block";
+        }
+    }
+    else {
+        const recipe = await getNewRandomRecipe();
+        
+        localStorage.setItem("@recipeHistory", JSON.stringify([...historyArr, recipe]));
+        localStorage.setItem("@recipeHistoryPoint", 0);
+
+        showResult(recipe); 
+    } 
 
 
 async function handleNext() {
